5e654b768733be09576bca8523acdcd611bbf0e2
sindhu.hosangadi <sindhu.hosangadi@redbus.com>
2023-11-18 12:19:14 +0530

    adding app store

--- netflixgpt/src/App.js
+++ netflixgpt/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Body from './Components/Body'
+import { Provider } from 'react-redux';
+import appStore from './Utils/appStore';
 
 function App() {
   return (
-    <div className="App">
+    
+ <Provider store={appStore}> 
   <Body />
-    </div>
+  </Provider> 
+    
   );
 }
 
